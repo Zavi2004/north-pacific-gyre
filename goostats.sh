78eaf974d6a787f4263568a34940e53846829ab8
Savindi Seneviratne <savindi.sene5152@gmail.com>
2026-04-23 14:51:38 +0800

    compute range

--- goostats.sh
+++ goostats.sh
@@ -4,7 +4,4 @@ echo "Working with ${fname}"
 echo "Welcome to Nelle's stst script"
 #Compute min/max/range of values in file
 min=$( cat ${fname} | sort | head -1)
-<<<<<<< HEAD
-=======
-
->>>>>>> 65f74b24eb5a7ef2c742ec047353543e38e15e1f
+range= $(echo "${max}-${min}" | bc -1
